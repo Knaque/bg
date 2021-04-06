94b771164ba605afaf15a51b6f710f188c77d554
Reilly Moore <williammoore6105@gmail.com>
2021-04-05 22:29:57 -0500

    improvements

--- src/bg.nim
+++ src/bg.nim
@@ -5,7 +5,8 @@ type
   Color = tuple[top, bottom: string]
   Rect = object
     color: Color
-    w, h, x, y, v: int
+    w, h, x, y, v, s: int
+    t: float
 
 const colors: array[4, Color] = [
   (top: "#F8A1EC", bottom: "#ED91CB"),
@@ -36,27 +37,35 @@ proc drawMain() =
     rects.add(
       Rect(
         color: colors[rand(0..3)],
-        w: rand(10..100),
-        h: rand(10..100),
-        x: rand(-9..1279),
+        w: rand(20..100),
+        h: rand(20..100),
+        x: rand(-19..1269),
         y: rand(720..1000),
-        v: rand(1..5)
+        v: rand(1..5),
+        s: rand(10..20),
+        t: rand(1.1..3.0)
       )
     )
   
   # render
   for i, rect in rects:
-    rectangle "bottom":
-      box rect.x, (rect.y.float + (rect.h.float / 2)), rect.w, rect.h.float / 2
-      fill rect.color.bottom
     rectangle "top":
-      box rect.x, rect.y, rect.w, rect.h
+      box rect.x, rect.y, rect.w, rect.h.float / rect.t
       fill rect.color.top
+    rectangle "bottom":
+      box rect.x, rect.y, rect.w, rect.h
+      fill rect.color.bottom
+    rectangle "shadow":
+      box rect.x, rect.y + rect.h, rect.w, rect.s
+      fill "#000000", 0.05
   rectangle "background":
     box 0, 0, 1280, 720
     fill "#F2F2F2"
 
 proc loadMain() =
   setTitle("pssm background")
+
+when defined(release):
+  echo "release :)"
 
 startFidget(drawMain, load=loadMain, w=1280, h=720, mainLoopMode=RepaintOnFrame)
